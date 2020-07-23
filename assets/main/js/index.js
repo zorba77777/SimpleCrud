4e60ad683ac592b8f0a467c3ab7cea65f580a8eb
Timur <sasintimur@gmail.com>
2020-07-24 00:33:02 +0300

    Bugs with added position were fixed

--- assets/main/js/index.js
+++ assets/main/js/index.js
@@ -2,7 +2,6 @@ $(function () {
 
     // Функция для сохранения позиции
     $('#save-position').on('click', function () {
-
             $.post(
                 '/main/add',
                 {
@@ -11,11 +10,18 @@ $(function () {
                 },
                 function (id) {
                     if (id !== null) {
+
+                        let lastTd = $($.parseHTML('<td class="remove" data-id=' + id +'><span class="fa fa-remove"></span></td>'));
+                        lastTd.on('click', remove);
+
                         let tr = $('<tr>');
+
                         tr.append($('<td>').text(id))
                             .append($('<td>').text($('#attr1').val()))
-                            .append($('<td>').text($('#attr2').val()));
+                            .append($('<td>').text($('#attr2').val()))
+                            .append(lastTd);
                         $("#table").find('tbody').append(tr);
+
                     } else {
                         alert('Не удалось добавить позицию');
                     }
@@ -23,24 +29,27 @@ $(function () {
             );
     });
 
-    $('.remove').on('click', function () {
-        let $this = $(this);
-        $.post(
-            '/main/remove',
-            {
-                id: $this.data('id')
-            },
-            function (data) {
-                if (data === 'success') {
-                    $this.parent().remove();
-                } else {
-                    alert('Не удалось удалить позицию');
-                }
-            }
-        );
-    });
+    $('.remove').on('click', remove);
 
 });
+
+// Функция для удаления позиции
+function remove() {
+    let $this = $(this);
+    $.post(
+        '/main/remove',
+        {
+            id: $this.data('id')
+        },
+        function (data) {
+            if (data === 'success') {
+                $this.parent().remove();
+            } else {
+                alert('Не удалось удалить позицию');
+            }
+        }
+    );
+}
 
 
 
